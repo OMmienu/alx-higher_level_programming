a09e2876d80f28291851d3990ca837a61f839394
OMmienu <nkedzi@gmail.com>
2023-01-27 11:30:48 +0000

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python3
-
 """
-Text indentation module
+Write a function that prints a text with 2 new lines after each of these
+characters: ., ? and :
 """
 
 
 def text_indentation(text):
-    """Prints a text with identation.
-    Args:
-        text (:obj: `str`): The text to print.
+    """prints a string. the output should have two line breaks after a '.', ':'
+    or '?'.
+    Parameter:
+    - text: text to print
     Raises:
-        TypeError: If `text` is not a string.
+    - TypeError: text is not a string
     """
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-    for c in text:
-        print(c, end='')
-        if c in ('.', '?', ':'):
-            print("\n")
+    if not isinstance(text, str):
+        raise(TypeError("text must be a string"))
+
+    output = ""
+    output = add_newLine(text, ".")
+    output = add_newLine(output, "?")
+    output = add_newLine(output, ":")
+
+    print(output, end='')
+
+def add_newLine(text, delim):
+    """Add new line characters after delimeter and strip any trailling whitty
+    space
+    Parameters:
+    - text: string to change delimeters
+    - delim: delimeter to separate text with
+    """
+    newStr = ""
+    text = text.lstrip(' ')
+    while True:
+        temp, delim, text = text.partition(delim)
+        if delim:
+            temp += f"{delim}\n\n"
+        newStr += temp
+        text = text.lstrip(' ')
+        if not text:
+            break
+
+    return newStr
