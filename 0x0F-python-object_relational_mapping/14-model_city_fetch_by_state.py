bfed52f38fb9e9985b1042ab1a883223b0fc03db
OMmienu <nkedzi@gmail.com>
2023-03-19 18:13:38 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" a Python file similar to model_state.py
+    named model_city.py that contains the class definition of a City
 """
+
 import sys
-from model_state import Base, State
 from model_city import City
+from model_state import State, Base
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    a, b, c = sys.argv[1], sys.argv[2], sys.argv[3]
+    data = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(data.format(a, b, c), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
+    result = session.query(City, State).join(City, State.id == City.state_id)
+    for i in result:
+        print(f"{i.State.name}: ({i.City.id}) {i.City.name}")
+    session.close()
