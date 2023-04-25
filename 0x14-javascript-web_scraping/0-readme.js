10c105cdccc23d06e1646122e97eb1b59aabe301
OMmienu <nkedzi@gmail.com>
2023-04-25 16:46:12 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 // Write a script that reads and prints the content of a file.
 
 const f = require('fs');
-const filenamme = processs.argv[2];
+const filenamme = process.argv[2];
 
 f.readFile(filename, 'utf-8', function (err, data) {
   if (err) console.error(err);
