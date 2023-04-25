b4f2b54da5d75a3e5d04eaec20e66e308ab50dde
OMmienu <nkedzi@gmail.com>
2023-04-25 16:55:44 +0000

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Write a script that writes a string to a file.
+
+const fs = require('fs');
+const filepath = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(filePath, content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
